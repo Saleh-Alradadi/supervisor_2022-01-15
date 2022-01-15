5a6ffc8d24efe6aaa8b4ce833d3363ca1981a4fb
Saleh-Alradadi <Saleh.Alradadi@gmail.com>
2022-01-15 19:47:43 +0300

    new

---
+++ app/src/main/java/com/example/supervisor_/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.supervisor_;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button random;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setTitle("Home");
+
+        random = (Button) findViewById(R.id.Random);
+
+        random.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this , Random.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+}
